8fe0620bfc8c8fb93f99e7f7564a5b113a3e15be
Steve Drew <steve@cloudaltitude.com>
2012-03-13 11:37:20 +0000

    update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def contact
+    @title = "Contact"
   end
   
   def about_us
+    @title = "About Us"
   end
     
 end
